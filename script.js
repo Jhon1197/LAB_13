9da4c5cf9bf16c13331b048b563afc13a6aed541
Jhon1197 <jhon.alvarez.ttc.771@unilibre.edu.co>
2024-11-14 22:08:52 -0500

    Calculadora

---
+++ script.js
@@ -0,0 +1,46 @@
+//funcion suma
+function suma(a,b){
+    return a+b;
+}
+//funcion de resta
+
+function resta(a,b){
+    return a-b;
+}
+//funcion multilicacion
+
+function multi(a,b){
+    return a*b;
+}
+//funcion de división
+
+function divi(a,b){
+    if (b==0){
+        return "error en la división  por cero"
+    }
+} return a/b;
+
+//funcion calculadora
+
+function calculadora(num1, num2, operacion){
+    switch(operacion){
+        case 'suma':
+            return suma(num1, num2);
+            case 'resta':
+            return resta(num1, num2);
+            case 'multi':
+            return multi(num1, num2);
+            case 'divi':
+                if(num2!==0){
+                    return divi(num1, num2);
+                }else
+            return 'error: División por cero';
+    }
+}    
+    let numero1=parseFloat(prompt("ingrese el numero 1"));
+    let numero2=parseFloat(prompt("Ingrese el numero 2"));
+    let operacion=prompt("Ingrese la operación (suma,resta,multi,divi)");
+
+    let resultado=calculadora(numero1,numero2,operacion);
+    document.write("El resulado es: " + resultado);
+
